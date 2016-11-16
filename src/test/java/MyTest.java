5cb8c5c175c77381560283ca2575baef518b0efb
Tirex <k580bm80@gmail.com>
2016-11-16 15:22:05 +0200

    Добавил проект. Задание 1

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+/**
+ * Created by Tirex on 16.11.2016.
+ */
+public class MyTest extends TestBase {
+
+    @Test
+    public void myFirstTest() {
+        driver.get("http://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("test");
+        driver.findElement(By.name("btnG")).click();
+        wait.until(titleIs("test - Поиск в Google"));
+
+        List<WebElement> list = driver.findElements(By.className("s"));
+        assertThat(9, equalTo(list.size()));
+
+
+    }
+
+
+
+}
+
---
+++ src/test/java/TestBase.java
@@ -0,0 +1,29 @@
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.HasCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Created by Tirex on 16.11.2016.
+ */
+public class TestBase {
+
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new FirefoxDriver();
+        System.out.println(((HasCapabilities) driver).getCapabilities());
+        wait = new WebDriverWait(driver, 10);
+
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
