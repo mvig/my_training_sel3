a53cf11b1b10decfae0c40250dc87b9763aecf15
Tirex <k580bm80@gmail.com>
2016-11-22 21:17:23 +0200

    Задание 6. Запускать Firefox Nightly
    Новая схема запуска FireFox  c указанием пути к установленному FireFox
    Nightly

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -25,9 +25,14 @@ public class TestBase {
         //driver = new InternetExplorerDriver();
 
         //Старая схема запуска FireFox  c указанием пути к установленному FireFox ESR 45.5.0
+        //DesiredCapabilities caps = new DesiredCapabilities();
+        //caps.setCapability(FirefoxDriver.MARIONETTE, false);
+        //driver =new FirefoxDriver(new FirefoxBinary(new File("C:\\Program Files\\Mozilla Firefox ESR45\\firefox.exe")),new FirefoxProfile(),caps);
+
+        //Новая схема запуска FireFox  c указанием пути к установленному FireFox Nightly
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability(FirefoxDriver.MARIONETTE, false);
-        driver =new FirefoxDriver(new FirefoxBinary(new File("C:\\Program Files\\Mozilla Firefox ESR45\\firefox.exe")),new FirefoxProfile(),caps);
+        caps.setCapability(FirefoxDriver.MARIONETTE, true);
+        driver =new FirefoxDriver(new FirefoxBinary(new File("C:\\Program Files\\Nightly\\firefox.exe")),new FirefoxProfile(),caps);
 
 
         System.out.println(((HasCapabilities) driver).getCapabilities());
