f04571a4929f9209c8d88be65d226f89cc99caba
Tirex <k580bm80@gmail.com>
2016-12-11 18:34:47 +0200

    Задание 14. Проверьте, что ссылки открываются в новом окне
    Сделайте сценарий, который проверяет, что ссылки на странице
    редактирования страны открываются в новом окне.
    Сценарий должен состоять из следующих частей:
    1) зайти в админку
    2) открыть пункт меню Countries (или страницу
    http://localhost/litecart/admin/?app=countries&doc=countries)
    3) открыть на редактирование какую-нибудь страну или начать создание
    новой
    4) возле некоторых полей есть ссылки с иконкой в виде квадратика со
    стрелкой -- они ведут на внешние страницы и открываются в новом окне,
    именно это и нужно проверить.
    Конечно, можно просто убедиться в том, что у ссылки есть атрибут
    target="_blank". Но в этом упражнении требуется именно кликнуть по
    ссылке, чтобы она открылась в новом окне, потом переключиться в новое
    окно, закрыть его, вернуться обратно, и повторить эти действия для всех
    таких ссылок.
    Не забудьте, что новое окно открывается не мгновенно, поэтому требуется
    ожидание открытия окна.

---
+++ src/test/java/TestOpenInNewWindow.java
@@ -0,0 +1,65 @@
+import org.apache.commons.lang3.RandomUtils;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+
+/**
+ * Created by Tirex on 11.12.2016.
+ */
+public class TestOpenInNewWindow extends TestBase {
+    @Test
+    public void addNewProductTest() throws InterruptedException {
+        doLiginByAdmin();
+        driver.navigate().to("http://localhost/litecart/admin/?app=countries&doc=countries");
+        wait.until(visibilityOfElementLocated(By.id("content")));
+        List<WebElement> editLink = driver.findElements(By.xpath("//a[@title='Edit']"));//получить список урл на редатирование страны
+        System.out.println("Всего стран: " + editLink.size());
+        getRandomItem(editLink.size(), editLink).click(); //открыть на редактирование слоучайно-выбранную страну
+        List<WebElement> forClick = driver.findElements(By.xpath("//a[@target='_blank']"));
+        System.out.println("Ссылок открывающих новое окно у с страницы редактирования" + forClick.size());
+        for (WebElement l : forClick) {
+            System.out.println(l);
+            newBrowserWindowDoOenClose(l);
+        }
+    }
+
+    public static WebElement getRandomItem(int amount, final List<WebElement> list) {
+        //Возвращаем случайный элемент списка
+        return list.get(RandomUtils.nextInt(0, amount - 1));
+
+    }
+
+    public void newBrowserWindowDoOenClose(WebElement l) throws InterruptedException {
+        String mainWindow = driver.getWindowHandle();
+        Set<String> oldWindowsSet = driver.getWindowHandles();
+        l.click(); // открывает новое окно
+        //sleep (5000);
+        // ожидаем открытия и получаем дескриптор нового окна
+        String newWindowHandle = (new WebDriverWait(driver, 10))
+                .until(new ExpectedCondition<String>() {
+                           public String apply(WebDriver driver) {
+                               // получаем новый набор дескрипторов, включающий уже и новое окно
+                               Set<String> newWindowsSet = driver.getWindowHandles();
+                               newWindowsSet.removeAll(oldWindowsSet);
+                               // получаем дескриптор нового окна
+                               return newWindowsSet.size() > 0 ?
+                                       newWindowsSet.iterator().next() : null;
+                           }
+                       }
+                );
+
+        driver.switchTo().window(newWindowHandle);
+        driver.close();
+        driver.switchTo().window(mainWindow);
+    }
+
+
+}
